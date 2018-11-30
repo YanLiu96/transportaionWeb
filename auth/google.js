3e2261ff3cbd2ba2614f77ae4aa07a4cbe8fc1c3
Yan Liu <278899085@qq.com>
2018-11-30 18:33:27 +0000

    fix error

--- auth/google.js
+++ auth/google.js
@@ -5,7 +5,7 @@ var User = require('../models/User');
 passport.use(new GoogleStrategy({
         clientID: "916453518762-fmceba3use5at73lvncmj5hhn7unc96g.apps.googleusercontent.com",
         clientSecret: "yQkIaEkga0RoI0q5xzArEULo",
-        callbackURL: "https://express-transportation.herokuapp.com/auth/google/callbacks"
+        callbackURL: "https://express-transportation.herokuapp.com/auth/google/callback"
     },
     function(accessToken, refreshToken, profile, done) {
         User.findOrCreate({ userid: profile.id }, { name: profile.displayName,userid: profile.id }, function (err, user) {
