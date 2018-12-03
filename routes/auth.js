2e6b419a5d1dc61430995ce3c1a146774d53b093
Yan Liu <278899085@qq.com>
2018-12-03 10:47:53 +0000

    fix login error

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var passportFacebook = require('../auth/facebook');
 //var passportTwitter = require('../auth/twitter');
 var passportGoogle = require('../auth/google');
-//var passportGitHub = require('../auth/github');
+var passportGitHub = require('../auth/github');
 /* LOGIN ROUTER */
 router.get('/login', function(req, res, next) {
     res.render('login', { title: 'Please Sign In with:' });
